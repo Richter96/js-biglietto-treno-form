7039feb962c948e72f724ac7a209f9d174d25198
Richter96 <castiglione96.rc@gmail.com>
2023-03-09 17:26:39 +0100

    add js

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,16 +8,6 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. 
 */
 
-
-/* 
-Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. 
-*/
-
 // strumenti
 
 // prompt
@@ -26,9 +16,14 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio,
 // division
 // modulus
 
+const generateBtn = document.getElementById("generate")
+console.log (generateBtn)
+
+/* 
+
 // chiedere età e i km da percorrere.
-const userAge = parseInt(prompt("Quale è la tua età?"));
-const kmTravel = prompt("Quanti km dovrai percorrere?");
+const userAge = parseInt(("Quale è la tua età?"));
+const kmTravel = ("Quanti km dovrai percorrere?");
 
 // caricare consol.log
 console.log(userAge);
@@ -41,7 +36,6 @@ console.log(priceTiket);
 
 
 let discountPrice = priceTiket;
-
 
 // va applicato uno sconto del 20% per i minorenni
 if (userAge < 18) {
@@ -60,4 +54,4 @@ if (isNaN(endPrice)) {
     alert('ciao, devi inserire nei campi dei numeri e non dei testi!!!')
 } else {
     document.getElementById("price_tiket").innerHTML = `Questo è il prezzo del biglietto: ${endPrice}€` 
-}
+} */
